710471aa6a09d4da92a7ff2324d0bdb30bafd89b
Wolonge <he@hedeiMac.local>
2015-08-13 16:57:28 +0800

    Initial Commit

---
+++ emojiKeyBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  emojiKeyBoard
+//
+//  Created by Wolonge on 15/8/13.
+//  Copyright (c) 2015年 Wolonge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ emojiKeyBoard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  emojiKeyBoard
+//
+//  Created by Wolonge on 15/8/13.
+//  Copyright (c) 2015年 Wolonge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
